b0b9006d579ebfecc39a59f8f098fafadd472634
mickael.morel <micmo35@hotmail.fr>
2017-03-17 10:01:38 +0100

    First commit

---
+++ sample_code/classifier.py
@@ -0,0 +1,32 @@
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
+
+
+class Classifier(BaseEstimator):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        self.clf =  KNeighborsClassifier()
+        self.clf.fit(X, y)
+
+    def predict(self, X):
+        return self.clf.predict(X)
+
+    def predict_proba(self, X):
+        return self.clf.predict_proba(X) # The classes are in the order of the labels returned by get_classes
+        
+    def get_classes(self):
+        return self.clf.classes_
+        
+    def save(self, path="./"):
+        pickle.dump(self, open(path + '_model.pickle', "w"))
+
+    def load(self, path="./"):
+        self = pickle.load(open(path + '_model.pickle'))
+        return self
